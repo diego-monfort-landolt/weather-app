3fcba2753017925f0553ef34af536ef1670cca08
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-12-19 16:12:38 +0100

    rm in eslint any - ad base to the vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://diego-monfort-landolt.github.io/weather-app',
 })
